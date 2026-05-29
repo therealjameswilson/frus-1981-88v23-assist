8dea1c393909d634dd28120d19b9291304ee8bd0
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 06:58:58 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 5,
+  "doc_id": "frus1981-88v23",
+  "title": "1981\u20131988, Volume XXIII, Iran-Contra Affair, 1985\u20131988",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1981-88v23",
+  "repo_slug": "frus-1981-88v23-assist",
+  "title_ascii": "1981-1988, Volume XXIII, Iran-Contra Affair, 1985-1988",
+  "pages_url": "https://therealjameswilson.github.io/frus-1981-88v23-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1981-88v23-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=XXIII+OR+Iran-Contra+OR+Affair&from=1981&to=1988&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
